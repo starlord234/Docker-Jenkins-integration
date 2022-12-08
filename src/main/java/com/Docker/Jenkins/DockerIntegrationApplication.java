82d9178b350fff5e6fed25c5601ef9ef74ceb0c9
starlord234 <jshaik830@gmail.com>
2022-12-08 18:57:55 +0530

    first commit

---
+++ src/main/java/com/Docker/Jenkins/DockerIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.Docker.Jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Docker/Jenkins/DockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Docker.Jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
